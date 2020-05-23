33bc4448476eff1522609cd72b2ca334a92ffd08
Jose Hurtado <hurtadojose89@gmail.com>
2020-05-24 01:51:11 +0200

    [2] - funcionality ok

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var boardClass = require('../lib/BoardService');
+const checkBoardStatus = require('../services/boardService');
+const getComputerNextMovement = require('../services/computerMoveService');
 
-router.post('/computerMove', function(req, res, next) {
+router.post('/computerMove', (req, res, next) => {
   const {board} = req.body;
-
-  board[0] = 'o';
-  const boardService = new boardClass(board);
-  console.log(boardService.toString());
+  const nextComputerPositionIndex = getComputerNextMovement(board);
+  board[nextComputerPositionIndex] = 'c';
+  const newBoard = board;
 
   setTimeout(() => {
-    return res.status(200).json({board});
+    return res.status(200).json({newBoard});
   }, 1000);
+});
+
+
+router.post('/checkBoardStatus', (req, res, next) => {
+  const {board} = req.body;
+  const response = checkBoardStatus(board);
+
+  return res.status(response.code).json({status: response.status})
+
 });
 
 module.exports = router;
